a9888510496bcbcc6aebeb4da26510c6d40f6bba
charlesmurenzi44 <charlesmurenzi44@gmail.com>
2026-06-03 02:23:03 +0200

    fix: correct import order in asgi.py

--- gigapp/asgi.py
+++ gigapp/asgi.py
@@ -1,13 +1,17 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gigapp.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gigapp.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
